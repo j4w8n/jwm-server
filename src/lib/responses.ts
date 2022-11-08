3f20f50fe3fcb24f7abaedc51a110d9dcb4f24ee
Jason Creviston <jcreviston@pm.me>
2022-11-08 17:13:09 -0500

    data first

--- src/lib/responses.ts
+++ src/lib/responses.ts
@@ -1,2 +1,2 @@
-export const InvalidJson = { error: 'Message: Invalid json.', data: { status: 'rejected' } }
-export const BodyNull = { error: 'Message: Body is null.', data: { status: 'rejected' } }
+export const InvalidJson = { data: { status: 'rejected', error: 'Message: Invalid json.' } }
+export const BodyNull = { data: { status: 'rejected' }, error: 'Message: Body is null.' }
