afe733b38db714cc6de98ff519ceb2759e7e15db
Jason Creviston <jason@malynium.com>
2023-01-03 10:12:58 -0500

    tatu

--- supabase/functions/_shared/constants.ts
+++ supabase/functions/_shared/constants.ts
@@ -1,2 +1,5 @@
+/* algorithm for signing messages and creating KeyLike objects for verifying messages */
 export const ALG = 'ES256'
+
+/* this server's domain */
 export const DOMAIN = 'boxbunnie.com'
